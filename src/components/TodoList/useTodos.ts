b133d41455fedf9cd2693511f1bb959e5a06e078
jgabriel1 <josegabriel@det.ufc.br>
2020-11-10 15:59:25 -0300

    Changed the todos state to have a reducer instead of a simple state.

--- src/components/TodoList/useTodos.ts
+++ src/components/TodoList/useTodos.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useReducer, Reducer } from 'react';
 import { useDatabaseConnection } from '../../data/connection';
 
 interface ITodo {
@@ -7,10 +7,63 @@ interface ITodo {
   status: boolean;
 }
 
+type ITodosReducer = Reducer<
+  {
+    todos: ITodo[];
+    createdTodo: ITodo | null;
+  },
+  | { type: 'SET_TODOS'; payload: ITodo[] }
+  | { type: 'SET_CREATED_TODO'; payload: ITodo }
+  | { type: 'ADD_CREATED_TODO_TO_LIST' }
+  | { type: 'DELETE_TODO'; payload: number }
+  | { type: 'TOGGLE_TODO'; payload: number }
+>;
+
 export default function useTodos() {
   const { todosRepository } = useDatabaseConnection();
 
-  const [todos, setTodos] = useState<ITodo[]>([]);
+  const [{ todos, createdTodo }, todosDispatch] = useReducer<ITodosReducer>(
+    (state, action) => {
+      switch (action.type) {
+        case 'SET_TODOS':
+          return {
+            ...state,
+            todos: action.payload,
+          };
+        case 'SET_CREATED_TODO':
+          return {
+            ...state,
+            createdTodo: action.payload,
+          };
+        case 'ADD_CREATED_TODO_TO_LIST':
+          if (state.createdTodo) {
+            return {
+              ...state,
+              todos: [...state.todos, state.createdTodo],
+              createdTodo: null,
+            };
+          }
+          return state;
+        case 'DELETE_TODO':
+          return {
+            ...state,
+            todos: state.todos.filter(todo => todo.id !== action.payload),
+          };
+        case 'TOGGLE_TODO':
+          return {
+            ...state,
+            todos: state.todos.map(todo =>
+              todo.id === action.payload
+                ? { ...todo, status: !todo.status }
+                : todo,
+            ),
+          };
+        default:
+          return state;
+      }
+    },
+    { todos: [], createdTodo: null },
+  );
 
   const addTodo = useCallback(
     async ({ text }: Pick<ITodo, 'text'>) => {
@@ -19,16 +72,20 @@ export default function useTodos() {
         status: false,
       });
 
-      setTodos(current => [...current, newTodo]);
+      todosDispatch({ type: 'SET_CREATED_TODO', payload: newTodo });
     },
     [todosRepository],
   );
+
+  const addCreatedTodoToList = useCallback(() => {
+    todosDispatch({ type: 'ADD_CREATED_TODO_TO_LIST' });
+  }, []);
 
   const deleteTodo = useCallback(
     async (todoId: number) => {
       await todosRepository.delete(todoId);
 
-      setTodos(current => current.filter(todo => todo.id !== todoId));
+      todosDispatch({ type: 'DELETE_TODO', payload: todoId });
     },
     [todosRepository],
   );
@@ -37,22 +94,22 @@ export default function useTodos() {
     async (todoId: number) => {
       await todosRepository.toggleCompleted(todoId);
 
-      setTodos(current =>
-        current.map(todo =>
-          todo.id === todoId ? { ...todo, status: !todo.status } : todo,
-        ),
-      );
+      todosDispatch({ type: 'TOGGLE_TODO', payload: todoId });
     },
     [todosRepository],
   );
 
   useEffect(() => {
-    todosRepository.getAll().then(setTodos);
+    todosRepository.getAll().then(loadedTodos => {
+      todosDispatch({ type: 'SET_TODOS', payload: loadedTodos });
+    });
   }, [todosRepository]);
 
   return {
     todos,
+    createdTodo,
     addTodo,
+    addCreatedTodoToList,
     deleteTodo,
     toggleTodo,
   };
