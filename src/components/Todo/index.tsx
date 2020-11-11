8f94916d0ccffa51f22a7159d2926af0f5095212
jgabriel1 <josegabriel@det.ufc.br>
2020-11-11 18:32:22 -0300

    Small cleanup refactor.

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View, Dimensions } from 'react-native';
 import Animated from 'react-native-reanimated';
 import { Feather } from '@expo/vector-icons';
