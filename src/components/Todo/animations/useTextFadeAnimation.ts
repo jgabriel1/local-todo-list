22a50403e05141931e8cf8f0df883a3a26d2c12a
jgabriel1 <josegabriel@det.ufc.br>
2020-11-11 18:31:12 -0300

    Added text opacity chenge when checkbox gets checked/unchecked.

---
+++ src/components/Todo/animations/useTextFadeAnimation.ts
@@ -0,0 +1,32 @@
+import { useCallback, useEffect } from 'react';
+import {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+
+export default function useTextFadeAnimation(isChecked: boolean) {
+  const opacity = useSharedValue(isChecked ? 1 : 0.3);
+
+  const style = useAnimatedStyle(() => {
+    return {
+      opacity: withTiming(opacity.value, {
+        duration: 200,
+        easing: Easing.ease,
+      }),
+    };
+  });
+
+  useEffect(() => {
+    if (isChecked) {
+      opacity.value = 0.3;
+    } else {
+      opacity.value = 1;
+    }
+  }, [isChecked, opacity.value]);
+
+  return {
+    style,
+  };
+}
