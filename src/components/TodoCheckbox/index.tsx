c2f226b82849a5bd31d7b5bd3fe9b00667def9d4
jgabriel1 <josegabriel@det.ufc.br>
2020-11-11 15:28:48 -0300

    Changed checkbox to a class component and added the animation.

--- src/components/TodoCheckbox/index.tsx
+++ src/components/TodoCheckbox/index.tsx
@@ -1,24 +1,73 @@
 import React from 'react';
-import { Feather } from '@expo/vector-icons';
+import { Animated, Easing } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
+import Lottie from 'lottie-react-native';
 
 import styles from './styles';
+
+import checkAnimation from '../../assets/check.json';
 
 interface TodoCheckboxProps {
   value: boolean;
   onToggle: () => void | Promise<void>;
 }
 
-const TodoCheckbox: React.FC<TodoCheckboxProps> = ({ value, onToggle }) => {
-  return (
-    <BorderlessButton onPress={onToggle} style={styles.container}>
-      {value ? (
-        <Feather name="check" size={28} color="#61d461" />
-      ) : (
-        <Feather name="circle" size={28} color="#6161d4" />
-      )}
-    </BorderlessButton>
-  );
-};
+interface TodoCheckboxState {
+  progress: Animated.Value;
+}
+
+class TodoCheckbox extends React.Component<
+  TodoCheckboxProps,
+  TodoCheckboxState
+> {
+  constructor(props: TodoCheckboxProps) {
+    super(props);
+
+    const { value } = this.props;
+
+    this.state = {
+      progress: new Animated.Value(value ? 0.75 : 0.35),
+    };
+  }
+
+  playAnimation = () => {
+    const { progress } = this.state;
+    const { value } = this.props;
+
+    Animated.timing(progress, {
+      toValue: value ? 0.35 : 0.75,
+      duration: 500,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  toggleCheckbox = () => {
+    const { onToggle } = this.props;
+
+    this.playAnimation();
+    onToggle();
+  };
+
+  render() {
+    const { progress } = this.state;
+
+    return (
+      <BorderlessButton
+        onPress={this.toggleCheckbox}
+        style={[styles.container, { padding: 0 }]}
+      >
+        <Lottie
+          source={checkAnimation}
+          style={{ height: 75, width: 75 }}
+          resizeMode="contain"
+          autoSize
+          duration={2000}
+          progress={progress}
+        />
+      </BorderlessButton>
+    );
+  }
+}
 
 export default TodoCheckbox;
