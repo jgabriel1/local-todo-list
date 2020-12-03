4b3c9be3d85e27891645aa3a3fcad9903b07cba4
jgabriel1 <josegabriel@det.ufc.br>
2020-12-02 23:43:14 -0300

    Modified router component to recieve the new hook.

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,6 +1,12 @@
 import React, { useCallback, useState } from 'react';
+import Animated from 'react-native-reanimated';
+
 import TodoList from '../TodoList';
 import TodoListsCatalog from '../TodoListsCatalog';
+
+import useNavigateSlideAnimation from './animations/useNavigateSlideAnimation';
+
+import styles from './styles';
 
 interface SelectedList {
   id: number;
@@ -9,6 +15,11 @@ interface SelectedList {
 
 const Routes: React.FC = () => {
   const [selectedList, setSelectedList] = useState<SelectedList | null>(null);
+
+  const {
+    animationFinished,
+    style: containerAnimatedStyle,
+  } = useNavigateSlideAnimation(!!selectedList);
 
   const navigateToList = useCallback((id: number, name: string) => {
     setSelectedList({ id, name });
@@ -19,17 +30,18 @@ const Routes: React.FC = () => {
   }, []);
 
   return (
-    <>
-      {selectedList ? (
+    <Animated.View style={[styles.container, containerAnimatedStyle]}>
+      {selectedList && animationFinished && (
         <TodoList
           listId={selectedList.id}
           listName={selectedList.name}
           returnToCatalog={returnToCatalog}
         />
-      ) : (
+      )}
+      {!selectedList && animationFinished && (
         <TodoListsCatalog navigateToList={navigateToList} />
       )}
-    </>
+    </Animated.View>
   );
 };
 
